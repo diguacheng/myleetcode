66200024a6b59605c4f3f52c387d40e4c7079930
diguacheng <467821528@qq.com>
2020-07-28 23:18:44 +0800

    add solution of  No.104

---
+++ 104二叉树的最大深度/main.go
@@ -0,0 +1,25 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root != nil {
+		return max(maxDepth(root.Left), maxDepth(root.Right))+1
+	}
+	return 0
+
+}
+
+func max(a,b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func main() {
+
+}
