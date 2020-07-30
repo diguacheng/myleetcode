4c2a938809a78a5736f4db57d32fdb8a5c037bec
diguacheng <467821528@qq.com>
2020-07-30 18:34:37 +0800

    add the solotion of no.100

---
+++ 100相同的树/main.go
@@ -0,0 +1,25 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p != nil && q != nil {
+		if p.Val == q.Val {
+			return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+		}
+		return false
+	}
+	if p==nil&&q==nil{
+		return true
+	}
+	return false
+
+}
+
+func main() {
+
+}
