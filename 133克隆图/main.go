2a72c574eee4b47117d358a380ea523cfccd5e3d
diguacheng <467821528@qq.com>
2020-08-05 22:59:40 +0800

    add dfs solution

--- 133克隆图/main.go
+++ 133克隆图/main.go
@@ -42,9 +42,45 @@ func cloneGraph(node *Node) *Node {
 	}
 	res := BFS(node)
 	return res
-
 }
 
+
+func cloneGraph1(node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+	visited := make(map[*Node]bool)
+	mapNode := make(map[int]*Node)
+	var getNode func(int) *Node
+	getNode = func(i int) *Node {
+		node, ok := mapNode[i]
+		if !ok {
+			node = &Node{Val: i, Neighbors: make([]*Node, 0)}
+			mapNode[i] = node
+		}
+		return node
+	}
+
+	var DFS func(node *Node) *Node
+	DFS = func(node *Node) *Node {
+		_, ok := visited[node]
+		if !ok {
+			newNode := getNode(node.Val)
+			for _, n := range node.Neighbors {
+				neighbor := getNode(n.Val)
+				newNode.Neighbors = append(newNode.Neighbors, neighbor)
+				visited[node] = true
+				DFS(n)
+			}
+			
+			
+			return newNode
+		}
+		return nil
+	}
+	res := DFS(node)
+	return res
+}
 func main() {
 	nodes := make([]*Node, 4)
 	for i := 0; i < 4; i++ {
@@ -54,7 +90,7 @@ func main() {
 	nodes[1].Neighbors = append(nodes[1].Neighbors, nodes[0], nodes[2])
 	nodes[2].Neighbors = append(nodes[2].Neighbors, nodes[1], nodes[3])
 	nodes[3].Neighbors = append(nodes[3].Neighbors, nodes[0], nodes[2])
-	x := cloneGraph(nodes[0])
+	x := cloneGraph1(nodes[0])
 	fmt.Println(x)
 
 }
