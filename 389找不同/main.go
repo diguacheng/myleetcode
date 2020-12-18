397f79b16e8b77c6813992cca549b6154c1b85fc
diguacheng <467821528@qq.com>
2020-12-18 13:02:03 +0800

    389

---
+++ 389找不同/main.go
@@ -0,0 +1,23 @@
+package main
+
+func findTheDifference(s string, t string) byte {
+    l:=[26]int{}
+    n:=len(s)
+    i:=0 
+    for i<n{
+        l[s[i]-'a']--
+        l[t[i]-'a']++
+        i++
+    }
+    l[t[i]-'a']++
+    for i=0;i<26;i++{
+        if l[i]==1{
+            return byte(i+'a')
+        }
+    }
+    return ' '
+}
+
+func main(){
+	
+}
