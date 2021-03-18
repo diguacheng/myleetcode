041acd7307066c24ce2b836e0f3145bef1c4e17e
diguacheng <467821528@qq.com>
2021-03-18 22:43:25 +0800

    s

---
+++ 200-299/297二叉树的序列化与反序列化/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+
+
+
+
+type Codec struct {
+}
+
+func Constructor() Codec {
+	return Codec{}
+}
+
+// Serializes a tree to a single string.
+func (this *Codec) serialize(root *TreeNode) string {
+	return rserialize(root, "")
+
+}
+
+func rserialize(root *TreeNode, str string) string {
+	if root == nil {
+		str += "nil,"
+		return str
+	}
+
+	str += strconv.Itoa(root.Val) + ","
+	str = rserialize(root.Left, str)
+	str = rserialize(root.Right, str)
+	return str
+}
+
+// Deserializes your encoded data to tree.
+func (this *Codec) deserialize(data string) *TreeNode {
+	dataSlice := strings.Split(data, ",")
+	dataSlice = dataSlice[:len(dataSlice)-1]
+	
+	root, _ := rdeserialize(dataSlice, 0)
+	return root
+}
+
+func rdeserialize(l []string, i int) (*TreeNode, int) {
+
+	if l[i] == "nil" {
+		i++
+		return nil, i
+	}
+	num, _ := strconv.Atoi(l[i])
+
+	root := &TreeNode{Val:num}
+	
+	var a,b int
+	root.Left, a = rdeserialize(l, i+1)
+	root.Right, b = rdeserialize(l, a)
+	return root, b
+
+}
+
+
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * obj := Constructor();
+ * data := obj.serialize(root);
+ * ans := obj.deserialize(data);
+ */
+
+func main() {
+	s := []string{"1", "2", "3", "nil", "nil", "4", "nil", "nil", "nil"}
+	fmt.Println(rdeserialize(s, 0))
+	de := "1,2,3,4,5,6,7,,"
+	dd := strings.Split(de, ",")
+	fmt.Println(dd, len(dd))
+
+}
---
+++ 400-799/674最长连续递增序列/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func findLengthOfLCIS(nums []int) int {
+	n := len(nums)
+	if n <= 1 {
+		return n
+	}
+	start := 0
+	max := 0
+	var i int
+	for i = 1; i < n; i++ {
+		if nums[i] <= nums[i-1] {
+			if max < i-start {
+				max = i - start
+			}
+			start = i
+		}
+	}
+	if i-start>max{
+		max=i-start
+	}
+	return max
+}
+
+func main() {
+	s := []int{1, 3, 5, 4, 7}
+	fmt.Println(findLengthOfLCIS(s))
+}
